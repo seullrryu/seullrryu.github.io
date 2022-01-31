bbe714b8c83f18935afd7add86382cf4ce6b81f5
seullrryu <sr4632@nyu.edu>
2022-01-30 23:50:01 -0500

    idk just pushing changes

--- scripts/deploy-github.js
+++ scripts/deploy-github.js
@@ -1,11 +1,11 @@
-const ghpages = require('gh-pages'); 
+const ghpages = require("gh-pages")
 ghpages.publish(
-    'public',
-    {
-      branch: 'master',
-      repo: 'https://github.com/seullrryu/seullrryu.github.io.git',
-    },
-    () => {
-      console.log('Deploy Complete!')
-    }
+  "public",
+  {
+    branch: "main",
+    repo: "https://github.com/seullrryu/seullrryu.github.io.git",
+  },
+  () => {
+    console.log("Deploy Complete!")
+  }
 )
